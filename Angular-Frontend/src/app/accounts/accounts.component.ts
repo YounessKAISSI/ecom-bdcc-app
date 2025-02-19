0b358a3bdce40db8454ce8767903d16da1a5d237
Youness KAISSI <ykaissi@outlook.com>
2025-02-19 10:47:45 +0100

    5th Commit (Frontend)

---
+++ Angular-Frontend/src/app/accounts/accounts.component.ts
@@ -0,0 +1,31 @@
+import {Component, OnInit} from '@angular/core';
+import {HttpClient, HttpClientModule} from "@angular/common/http";
+import {NgFor, NgIf} from "@angular/common";
+
+@Component({
+  selector: 'app-accounts',
+  standalone: true,
+  imports: [HttpClientModule,NgFor,NgIf],
+  templateUrl: './accounts.component.html',
+  styleUrl: './accounts.component.css'
+})
+export class AccountsComponent implements OnInit{
+
+    accounts : any;
+
+    constructor(private http : HttpClient) {
+    }
+
+    ngOnInit(): void {
+      this.http.get("http://localhost:8888/ACCOUNT-SERVICE/accounts")
+        .subscribe({
+          next : data  => {
+            this.accounts = data;
+          },
+          error : err => {
+            console.log(err);
+          }
+        });
+    }
+
+}
